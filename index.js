d0d6d24a8373c7a4fa3bbe61244fe31604ce2321
Nickp125 <npink125@yahoo.com>
2026-05-08 13:02:24 -0400

    Exclusive

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
-// template_72c9hds
-//
-// thAwhzfkEPN9bQewN
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 /20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+  console.log(x, y)
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"        
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event) {
   event.preventDefault();
-  // emailjs
-  // .sendForm(
-  //     "service_5kib6jc",
-  //     "template_72c9hds",
-  //     event.target,
-  //     "thAwhzfkEPN9bQewN",
-  // )
-  // .then(() => {
-  //     console.log("this worked1");
-  // });
-  
-  const loading = document.querySelector('.modal__overlay--loading')
-  const success = document.querySelector('.modal__overlay--success')
-  loading.classList += " modal__overlay--visible"
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
 
+  emailjs
+    .sendForm(
+      "service_5kib6jc",
+      "template_72c9hds",
+      event.target,
+      "thAwhzfkEPN9bQewN",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly via email on npink125@yahoo.com",
+      );
+    });
+}
 
-  setTimeout(() => {
-    loading.classList.remove("modal__overlay--visible")
-    success.classList += " modal__overlay--visible"
-    console.log("it worked 1");
-  }, 1000);
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
